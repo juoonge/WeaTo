e624ed1093f4770214c85a41138e8089c6c89fcb
임태규 <kyu5787@naver.com>
2022-07-15 09:14:46 +0900

    파일 업로드 빼고 구현 완료

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class WeatherComment(models.Model):
+    location = models.CharField(max_length=30)
+    temperature = models.FloatField()  # 온도 저장용 field. 더움 / 적당함/ 추움 3가지로 구분
+    sky = models.SmallIntegerField()  # 하늘 저장용 field. 맑음 / 구름적음 / 구름많음 / 흐림 4가지로 구분
+    humidity = models.SmallIntegerField()  # 습기 저장용 field. 건조함 / 적당함 / 습함 으로 3가지 구분
+    wind = models.SmallIntegerField()  # 바람 저장용 field. 많이 안붐 / 적당함 / 많이 붐 3가지로 구분
+    rain = models.SmallIntegerField(default=0)  # 날씨 저장용 field. 아무것도 없음 / 비 / 눈 3가지로 구분하고, 아무것도 없는 상태를 기본값으로 지정
+    description = models.TextField()  # 날씨 묘사용 field.
+    scene = models.ImageField(blank=True, upload_to='scene/', default='default.png',)  # 사진 저장용 경로 설정.
+    date = models.DateTimeField(auto_now_add=True)  # 작성할때 자동으로 시간 저장.
+
+    def __str__(self):
+        return self.location + '\t' + str(self.date)
--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.generics import get_object_or_404
+import datetime
 
-# Create your views here.
+from rest_framework import viewsets
+
+from .models import WeatherComment
+from .serializer import WeatherCommentSerializer, WeatherCommentCreateSerializer
+from django.db.models import Q
+
+class TotalWeatherCommentAPIView(APIView):
+    def get(self, request):
+        total_weathercomment = WeatherComment.objects.all()
+        serializer = WeatherCommentSerializer(total_weathercomment, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class LocalWeatherCommentAPIView(APIView):
+    def get(self, request, location):
+        start_date = datetime.date.today()
+        end_date = start_date + datetime.timedelta(days=-1)
+
+        local_weathercomment = WeatherComment.objects.filter(Q(location=location) & Q(date__range=[end_date, start_date]))
+        serializer = WeatherCommentSerializer(local_weathercomment, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class WeatherCreateAPIView(APIView):
+    def post(self, request):
+        serializer = WeatherCommentCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
