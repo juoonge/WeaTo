156317e0aecb29f440b904b2e2313c10893f6892
AHNDOIL <ahj3579@naver.com>
2022-07-17 00:57:09 +0900

    JWT Authentication

--- users/models.py
+++ users/models.py
@@ -1,3 +1,68 @@
 from django.db import models
 
 # Create your models here.
+
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+
+class CustomAccountManager(BaseUserManager):
+
+    def create_user(self, email, user_name, password=None, password2=None, **other_fields):
+        if not email:
+            raise TypeError('Users should have an email.')
+        if not user_name:
+            raise TypeError('Users should hae a username.')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, user_name=user_name, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+    def create_superuser(self, email, user_name, password=None, **other_fields):
+
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must be assigned to is_staff=True.')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must be assigned to is_superuser=True.')
+
+        return self.create_user(email, user_name, password, **other_fields)
+
+
+
+
+
+class NewUser(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(max_length=255,unique=True)
+    user_name = models.CharField(max_length=100, unique=True)
+   
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    objects = CustomAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['user_name']
+
+    def __str__(self):
+        return self.user_name
+    
+    def tokens(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            'refresh':str(refresh),
+            'access': str(refresh.access_token)
+        }
