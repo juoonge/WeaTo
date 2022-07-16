1660c9f2cca2f4d8df30a98aaf269ff602dfad20
juoonge <juoonge7646@naver.com>
2022-07-17 00:50:38 +0900

    get location info & modify cctv func

--- config/settings.py
+++ config/settings.py
@@ -95,9 +95,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -107,7 +107,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
--- weather/urls.py
+++ weather/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.conf import settings
+from . import views
 from django.conf.urls.static import static
 
 from .views import TotalWeatherCommentAPIView, LocalWeatherCommentAPIView, WeatherCreateAPIView
 
 urlpatterns = [
+    path('',views.cctv,name='cctv'),
     path('total/', TotalWeatherCommentAPIView.as_view()),
     path('local/<str:location>/', LocalWeatherCommentAPIView.as_view()),
     path('create/', WeatherCreateAPIView.as_view()),
