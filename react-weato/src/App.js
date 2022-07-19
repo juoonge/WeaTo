662c06f746b4f09db22389b685ebb2dcb54b61c9
heejinnn <gml0593@gmail.com>
2022-07-20 02:08:52 +0900

    WeatoAdd page react

--- react-weato/src/App.js
+++ react-weato/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import {Routes,Route} from 'react-router-dom';
-import Login from './Login';
-import SignUp from './SignUp';
+//import {Routes,Route} from 'react-router-dom';
+// import Login from './Login';
+// import SignUp from './SignUp';
+import WeatoAdd from './WeatoAdd.js';
 import './index.css';
-
 function App() {
   return (
-    <Login/>
+    <WeatoAdd/>
     // <Routes>
     //   <Route path="/" element={<Login />}>
     //     <Route path="/SignUp" element={<SignUp />} />
