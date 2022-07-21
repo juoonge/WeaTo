27adbee1a6cdd9202a6ef0adf7d2c4c6bf4cae44
myone <201queen@naver.com>
2022-07-22 02:03:54 +0900

    ++

--- react-weato/src/App.js
+++ react-weato/src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 
 import Layout from './components/Layout/Layout';
-import {Routes,Route} from 'react-router-dom';
-import Login from './Login';
-import SignUp from './SignUp';
+
 function App() {
   return (
     <>
       <Layout />
-      <Routes>
-        <Route path="/Login" element={<Login />}/>
-        <Route path="/SignUp" element={<SignUp />} />
-      </Routes>
+
     </>
 
 // import React from "react";
