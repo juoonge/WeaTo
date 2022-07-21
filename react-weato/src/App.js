86021aa8f91e702df9540b07b2f448b556ef58b4
myone <o200queen10@gmail.com>
2022-07-21 17:29:27 +0900

    여러분 지송..

--- react-weato/src/App.js
+++ react-weato/src/App.js
@@ -1,17 +1,38 @@
-import React from "react";
+// import React from "react";
+// import {Routes,Route} from 'react-router-dom';
+// import Login from './Login';
+// import SignUp from './SignUp';
+// import './index.css';
+
+// function App() {
+//   return (
+//     <Login/>
+//     // <Routes>
+//     //   <Route path="/" element={<Login />}>
+//     //     <Route path="/SignUp" element={<SignUp />} />
+//     //   </Route>
+//     // </Routes>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+
+import Layout from './components/Layout/Layout';
 import {Routes,Route} from 'react-router-dom';
 import Login from './Login';
 import SignUp from './SignUp';
-import './index.css';
-
 function App() {
   return (
-    <Login/>
-    // <Routes>
-    //   <Route path="/" element={<Login />}>
-    //     <Route path="/SignUp" element={<SignUp />} />
-    //   </Route>
-    // </Routes>
+    <>
+      <Layout />
+      <Routes>
+        <Route path="/Login" element={<Login />}/>
+        <Route path="/SignUp" element={<SignUp />} />
+      </Routes>
+    </>
+
   );
 }
 
