d4931bc2aa3e87d132445810b6611d64468d3d15
heejinnn <gml0593@gmail.com>
2022-07-22 02:07:03 +0900

    WeatoHome/LeftBox/RightBox/ 그리고 css

--- react-weato/src/App.js
+++ react-weato/src/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
+// import React from 'react';
 
-import Layout from './components/Layout/Layout';
-import {Routes,Route} from 'react-router-dom';
-import Login from './Login';
-import SignUp from './SignUp';
-function App() {
-  return (
-    <>
-      <Layout />
-      <Routes>
-        <Route path="/Login" element={<Login />}/>
-        <Route path="/SignUp" element={<SignUp />} />
-      </Routes>
-    </>
+// import Layout from './components/Layout/Layout';
+// import {Routes,Route} from 'react-router-dom';
+// import Login from './Login';
+// import SignUp from './SignUp';
+// function App() {
+//   return (
+//     <>
+//       <Layout />
+//       <Routes>
+//         <Route path="/Login" element={<Login />}/>
+//         <Route path="/SignUp" element={<SignUp />} />
+//       </Routes>
+//     </>
+//   );
+// }
+// export default App;
+// 나원님 꺼
+
+
 
 // import React from "react";
 // //import {Routes,Route} from 'react-router-dom';
@@ -28,7 +34,18 @@ function App() {
 //     //     <Route path="/SignUp" element={<SignUp />} />
 //     //   </Route>
 //     // </Routes>
+//   );
+// }
 
+import React from 'react';
+
+import WeatoHome from './components/sections/WeatoHome';
+
+function App() {
+  return (
+    <>
+      <WeatoHome/>
+    </>
   );
 }
 
---
+++ react-weato/src/components/UI/LeftBox.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const ReftBox = () =>{
+    return (
+        <div className='leftBox'>
+            <div className="leftBox-layout">
+                <input type="text" name="place" placeholder="어느 지역의 날씨를 찾아볼까요?"></input>          
+            </div>
+            <div className="leftBox-layout">
+                
+            </div>
+            <div className="leftBox-layout">
+                
+            </div>
+            <div className="leftBox-layout">
+                
+            </div>
+        </div>
+    );
+};
+export default ReftBox;
