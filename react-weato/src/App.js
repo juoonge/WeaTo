d0f5d05df3bf4e455209a6e6f250dc114c3d5fbd
juoonge <juoonge7646@naver.com>
2022-07-21 23:29:15 +0900

    Revert "Commit 86021aa8f91e702df9540b07b2f448b556ef58b4"

--- react-weato/src/App.js
+++ react-weato/src/App.js
@@ -1,38 +1,17 @@
-// import React from "react";
-// import {Routes,Route} from 'react-router-dom';
-// import Login from './Login';
-// import SignUp from './SignUp';
-// import './index.css';
-
-// function App() {
-//   return (
-//     <Login/>
-//     // <Routes>
-//     //   <Route path="/" element={<Login />}>
-//     //     <Route path="/SignUp" element={<SignUp />} />
-//     //   </Route>
-//     // </Routes>
-//   );
-// }
-
-// export default App;
-
-import React from 'react';
-
-import Layout from './components/Layout/Layout';
+import React from "react";
 import {Routes,Route} from 'react-router-dom';
 import Login from './Login';
 import SignUp from './SignUp';
+import './index.css';
+
 function App() {
   return (
-    <>
-      <Layout />
-      <Routes>
-        <Route path="/Login" element={<Login />}/>
-        <Route path="/SignUp" element={<SignUp />} />
-      </Routes>
-    </>
-
+    <Login/>
+    // <Routes>
+    //   <Route path="/" element={<Login />}>
+    //     <Route path="/SignUp" element={<SignUp />} />
+    //   </Route>
+    // </Routes>
   );
 }
 
