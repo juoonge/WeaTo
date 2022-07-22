7fab580ceb02921716cf1d70cd19ce4be725216c
juoonge <juoonge7646@naver.com>
2022-07-22 21:10:24 +0900

    settings.py 수정

--- config/settings.py
+++ config/settings.py
@@ -89,7 +89,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:3000', 'http://localhost:3000')
+CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:8000', 'http://localhost:3000')
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'config.urls'
