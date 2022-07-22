cb20d9a1c50c17771b6ac3f92a26fc0e606e94fa
heejinnn <gml0593@gmail.com>
2022-07-22 19:24:19 +0900

    LeftBox 완성

--- react-weato/src/components/Today/WeatoAdd.js
+++ react-weato/src/components/Today/WeatoAdd.js
@@ -1,9 +1,8 @@
 import React, {useState} from 'react'
-import "../../static/style.css";
+import Addlayout from './Addlayout';
+import "../../static/css/WeatoAdd.css";
 import {Link} from 'react-router-dom';
 
-  // import { faUser } from "@fortawesome/free-solid-svg-icons";
-  // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function WeatoAdd() {
 let now= new Date();
@@ -26,46 +25,6 @@ const saveFileImage = (e) => {// 첨부한 사진 링크 가져오기
 setFileImage(URL.createObjectURL(e.target.files[0]));
 };
 
-const checkOnlyOne = (checkThis) => {
-const checkboxes = document.getElementsByName('temp')
-for (let i = 0; i < checkboxes.length; i++) {
-    if (checkboxes[i] !== checkThis) {
-    checkboxes[i].checked = false
-    }
-}
-}
-const checkOnlyOne1 = (checkThis) => {
-const checkboxes = document.getElementsByName('sky')
-for (let i = 0; i < checkboxes.length; i++) {
-    if (checkboxes[i] !== checkThis) {
-    checkboxes[i].checked = false
-    }
-}
-}
-const checkOnlyOne2 = (checkThis) => {
-const checkboxes = document.getElementsByName('hum')
-for (let i = 0; i < checkboxes.length; i++) {
-    if (checkboxes[i] !== checkThis) {
-    checkboxes[i].checked = false
-    }
-}
-}
-const checkOnlyOne3 = (checkThis) => {
-const checkboxes = document.getElementsByName('windy')
-for (let i = 0; i < checkboxes.length; i++) {
-    if (checkboxes[i] !== checkThis) {
-    checkboxes[i].checked = false
-    }
-}
-}
-const checkOnlyOne4 = (checkThis) => {
-const checkboxes = document.getElementsByName('rainOrsnow')
-for (let i = 0; i < checkboxes.length; i++) {
-    if (checkboxes[i] !== checkThis) {
-    checkboxes[i].checked = false
-    }
-}
-}
 
 const areaname="대구광역시 수성구 신매동";
 
@@ -102,108 +61,7 @@ return(
         </div>
         <div className="add-box">
             {/* 레이아웃 오른쪽 */}
-            <div className="add-box-right">
-            <div className="add-box-right-rayout">
-                <div className="add-box-right-title">· 오늘의 온도는</div>
-                <div className="add-box-right-state">
-                <input type="checkbox" id="hot" name="temp" onChange={(e) => checkOnlyOne(e.target)} ></input>
-                <label for="hot"></label>
-                <input type="checkbox" id="warm" name="temp" onChange={(e) => checkOnlyOne(e.target)} ></input>
-                <label for="warm"></label>
-                <input type="checkbox" id="cold"name="temp" onChange={(e) => checkOnlyOne(e.target)} ></input>
-                <label for="cold"></label>
-                </div>
-                <div className="add-box-right-img">
-                <img src="img/icon_weather_hot.png" alt="hot"/>
-                <img src="img/icon_weather_dry.png" alt="dry"/>
-                <img src="img/icon_weather_cold.png" alt="cold"/>
-                </div>
-                <div className="add-box-right-img-label">
-                <label>더움</label>
-                <label>적당</label>
-                <label>추움</label>
-                </div> 
-            </div>
-            <div className="add-box-right-rayout">
-                <div className="add-box-right-title">· 오늘의 하늘은</div>
-                <div className="add-box-right-state-sky">
-                <input type="checkbox" id="sunny" name="sky" onChange={(e) => checkOnlyOne1(e.target)}></input>
-                <label for="sunny"></label>
-                <input type="checkbox" id="cloudy1" name="sky" onChange={(e) => checkOnlyOne1(e.target)}></input>
-                <label for="cloudy1"></label>
-                <input type="checkbox" id="cloudy2"name="sky" onChange={(e) => checkOnlyOne1(e.target)}></input>
-                <label for="cloudy2"></label>
-                <input type="checkbox" id="cloudy3"name="sky" onChange={(e) => checkOnlyOne1(e.target)}></input>
-                <label for="cloudy3"></label>
-                </div>
-                <div className="add-box-right-img-sky">
-                <img src="img/icon_weather_sunny.png" alt="sunny"/>
-                <img src="img/icon_weather_cloudy1.png" alt="cloudy1"/>
-                <img src="img/icon_weather_cloudy2.png" alt="cloudy2"/>
-                <img src="img/icon_weather_cloudy3.png" alt="cloudy3"/>
-                </div>
-                <div className="add-box-right-img-label-sky">
-                <label>맑음</label>
-                <label>적음</label>
-                <label>많음</label>
-                <label>흐림</label>
-                </div>   
-            </div>
-            <div className="add-box-right-rayout">
-                <div className="add-box-right-title">· 오늘의 습도는</div>
-                <div className="add-box-right-state">
-                <input type="checkbox" id="dry" name="hum" onChange={(e) => checkOnlyOne2(e.target)}></input>
-                <label for="dry"></label>
-                <input type="checkbox" id="moderate" name="hum" onChange={(e) => checkOnlyOne2(e.target)}></input>
-                <label for="moderate"></label>
-                <input type="checkbox" id="wet"name="hum" onChange={(e) => checkOnlyOne2(e.target)}></input>
-                <label for="wet"></label>
-                </div>
-                <div className="add-box-right-img">
-                <img src="img/icon_weather_dry.png" alt="dry"/>
-                <img src="img/icon_weather_moderate_humidity.png" alt="moderate"/>
-                <img src="img/icon_weather_wet.png" alt="wet"/>
-                </div> 
-                <div className="add-box-right-img-label">
-                <label>건조</label>
-                <label>적당</label>
-                <label>습함</label>
-                </div> 
-            </div> 
-            <div className="add-box-right-rayout">
-                <div className="add-box-right-title">· 오늘의 바람은</div>
-                <div className="add-box-right-state">       
-                <input type="checkbox" id="windless" name="windy" onChange={(e) => checkOnlyOne3(e.target)}></input>
-                <label for="windless"></label>
-                <input type="checkbox" id="windy" name="windy" onChange={(e) => checkOnlyOne3(e.target)}></input>
-                <label for="windy"></label>
-                <input type="checkbox" id="typoon"name="windy" onChange={(e) => checkOnlyOne3(e.target)}></input>
-                <label for="typoon"></label>
-                </div>
-                <div className="add-box-right-img">
-                <img src="img/icon_weather_windless.png" alt="windless"/>
-                <img src="img/icon_weather_windy.png" alt="windy"/>
-                <img src="img/icon_weather_typhoon.png" alt="typoon"/>
-                </div>
-                <div className="add-box-right-img-label">
-                <label>적게</label>
-                <label>적당</label>
-                <label>많이</label>
-                </div>  
-            </div>
-            <div className="add-box-right-rayout">
-                <div className="add-box-right-rainOrsnow">
-                <input type="checkbox" id="rainy" name="rainOrsnow" onChange={(e) => checkOnlyOne4(e.target)}></input>
-                <label for="rainy"></label>
-                <img src="img/icon_weather_rainy.png" alt="rainy"/>
-                <label>비</label>
-                <input type="checkbox" id="snow" name="rainOrsnow" onChange={(e) => checkOnlyOne4(e.target)}></input>
-                <label for="snow"></label>
-                <img src="img/icon_weather_snowy.png" alt="snow"/>
-                <label>눈</label>
-                </div>
-            </div>             
-            </div>              
+            <Addlayout/>            
         </div>
         </div>
         {/* 코멘트 입력 공간 */}
--- react-weato/src/components/UI/LeftBox.js
+++ react-weato/src/components/UI/LeftBox.js
@@ -1,19 +1,58 @@
 import React from "react";
+import Leftlayout from './Leftlayout';
 
 const ReftBox = () =>{
+let now= new Date();
+let year=now.getFullYear();
+let todayMonth=now.getMonth()+1;
+let todayDate=now.getDate();
+const week=['일','월','화','수','목','금','토'];
+let datOfWeek=week[now.getDay()];
+let hours=now.getHours();
+let minutes=now.getMinutes();
+
     return (
         <div className='leftBox'>
             <div className="leftBox-layout">
-                <input type="text" name="place" placeholder="어느 지역의 날씨를 찾아볼까요?"></input>          
+                <img className="locate-icon" src="img/icon_search_locate.png"></img>
+                <input type="text" name="place" placeholder="어느 지역의 날씨를 찾아볼까요?"></input>
+                <button className="search-button"></button>
             </div>
             <div className="leftBox-layout">
-                
+                <div className="weather-content">
+                    <div className="weather-info">
+                       <img src="" alt="weather"></img> 
+                    </div>
+                    <div className="weather-info">
+                        <div className="today">
+                            {year}.{todayMonth}.{todayDate} ({datOfWeek}) {hours}:{minutes} 기준
+                        </div>
+                        <div className="area">
+                            대구광역시 수성구 신매동
+                        </div>
+                        <div className="avg-temp">
+                            26.9℃
+                        </div>
+                        <div className="avg-rainy">
+                            <div className="rainy-icon">강수</div>
+                            <div>34%</div>
+                        </div>
+                        <div className="avg-windy">
+                            <div className="windy-icon">풍속</div>
+                            <div>3m/s</div>
+                        </div>
+                        <div className="avg-hum">
+                            <div className="hum-icon">습도</div>
+                            <div>36%</div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className="leftBox-layout">
-                
+                <Leftlayout/>
             </div>
             <div className="leftBox-layout">
-                
+                표
             </div>
         </div>
     );
